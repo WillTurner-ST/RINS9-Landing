0f64a05f600e54c477eadf413b42d2d886fb947e
WillTurnerST <williamturner321@gmail.com>
2023-09-27 11:30:28 -0400

    Updated Footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,17 +1,22 @@
 import React from 'react'
+import { FaFacebook, FaInstagram } from 'react-icons/fa'
+import { FaTwitter } from 'react-icons/fa'
+import CTAbutton from '../assets/Images/CTAbutton1.png'
 
 const Footer = () => {
   return (
     <div className="cta-container">
-      <h2 className="cta-title">Get Started with Our Product!</h2>
+      <h3 className="cta-title">Get Started with Our Product!</h3>
       <p className="cta-description">Join us and explore the endless possibilities with our versatile product.</p>
-      <a href="#your-link" className="cta-button">Start Now</a>
+      <div className='cta-button'>
+           <a href='https://www.snapdragonhemp.com/product/canna-bliss-sparkling-infused-water-12oz-10mg-or-25mg-delta-9-thc/'><img className='CButton' alt='Call-action-button' src={CTAbutton}/></a>
+        </div>
       <div className="cta-socials">
-        <a href="#facebook" className="cta-social-link">FB</a> {/* Replace with Facebook icon */}
-        <a href="#twitter" className="cta-social-link">TW</a> {/* Replace with Twitter icon */}
-        <a href="#instagram" className="cta-social-link">IG</a> {/* Replace with Instagram icon */}
+        <a href="#facebook" className="cta-social-link"><FaFacebook/></a>
+        <a href="#twitter" className="cta-social-link"><FaTwitter/></a>
+        <a href="#instagram" className="cta-social-link"><FaInstagram/></a>
       </div>
-      <p className="cta-copyright">© 2023 Your Company Name. All rights reserved.</p>
+      <p className="cta-copyright">© 2023 Snapdragon Hemp. All rights reserved.</p>
     </div>
   );
 };
