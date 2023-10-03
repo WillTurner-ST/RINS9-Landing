77b78adde52bc6eefa096c4a2a6bde761277ce99
WillTurnerST <williamturner321@gmail.com>
2023-10-03 13:23:17 -0400

    About verbage

--- client/src/components/FAQ.js
+++ client/src/components/FAQ.js
@@ -14,7 +14,7 @@ const FAQ = () => {
     const FAQs = [
         { question: 'Is canna bliss alcohol free?', answer: 'Yes! Canna bliss does not contain any alcohol.' },
         { question: 'How does RINS9 work?', answer: 'RINS9 is a water-souluble powder that absorbs into water instead of fat.This means it can first be absorbed sublingually allowing for rapid and effective elevation.' },
-        { question: 'What makes RINS9 work?', answer: 'We start with quality hemp which is turned into a nano-sonic water-soluble form, providing clean elevation without the need for alcohol' },
+        { question: 'What makes RINS9 work?', answer: 'We start with quality materials from fresh flower which is turned into a nano-sonic water-soluble form, providing clean elevation without the need for alcohol.' },
         { question: 'Where can i buy it?', answer: <>Stop by one of our many Chattanooga locations, or shop online <a href='https://www.snapdragonhemp.com/product/canna-bliss-sparkling-infused-water-12oz-10mg-or-25mg-delta-9-thc/'>Here</a>!</>},
 
       ];
